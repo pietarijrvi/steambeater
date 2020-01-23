f0a0411e9e6ae665fb50e9bde5850a47dfedf706
Pietari Järvi <pietari.jarvi@gmail.com>
2020-01-23 11:56:55 +0200

    Update App.java
    Small changes

--- src/main/java/com/ryhma6/maven/steambeater/App.java
+++ src/main/java/com/ryhma6/maven/steambeater/App.java
@@ -3,20 +3,18 @@ package com.ryhma6.maven.steambeater;
 import model.Stat;
 import model.StatAccessObject;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
+    	// Testing local database
     	StatAccessObject statDAO = new StatAccessObject();
         String nimi = "Pekka";
 		String tunnus = "123";
 		Stat stat = new Stat(tunnus,nimi);
         if(statDAO.createStat(stat)) {
-        	System.out.println("Tallennettu tietokantaan !!!!");
+        	System.out.println("Tallennettu tietokantaan");
         }
     }
 }
