fbc724367c7da6a0db468f34fd6d8b155911a9ed
Pietari Järvi <pietari.jarvi@gmail.com>
2020-02-27 14:21:32 +0200

    jacoco changes

--- src/test/java/testiTesti.java
+++ src/test/java/testiTesti.java
@@ -3,11 +3,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class testiTesti {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 	
 	@Test
 	void test2() {
