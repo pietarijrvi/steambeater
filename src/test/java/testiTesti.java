bd443b1a9c3a36f161d34db388d91a45dc190b7a
Pietari Järvi <pietari.jarvi@gmail.com>
2020-02-27 13:48:37 +0200

    Create testiTesti.java
    testing jenkins

---
+++ src/test/java/testiTesti.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testiTesti {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void test2() {
+		assertTrue(true);
+	}
+
+}
