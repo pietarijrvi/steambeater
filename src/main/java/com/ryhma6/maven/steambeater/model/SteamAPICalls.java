84fc9359261f46a92b50367a36d7d79df5d8bdab
Jetro Saarti <jetro.saarti@metropolia.fi>
2020-02-28 22:57:57 +0200

    Friend information from Steam (api calls and java objects)

--- src/main/java/com/ryhma6/maven/steambeater/model/SteamAPICalls.java
+++ src/main/java/com/ryhma6/maven/steambeater/model/SteamAPICalls.java
@@ -10,14 +10,19 @@ import java.util.Scanner;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ryhma6.maven.steambeater.model.steamAPI.Friend;
+import com.ryhma6.maven.steambeater.model.steamAPI.FriendList;
 import com.ryhma6.maven.steambeater.model.steamAPI.GameData;
 import com.ryhma6.maven.steambeater.model.steamAPI.OwnedGames;
+import com.ryhma6.maven.steambeater.model.steamAPI.PlayerProfile;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class SteamAPICalls {
 	private static ObservableList<GameData> playerGames = FXCollections.observableArrayList();
+	private static ObservableList<Friend> friendList = FXCollections.observableArrayList();
+	
 	private ObjectMapper mapper = new ObjectMapper();
 	
 	public SteamAPICalls() {
@@ -30,6 +35,10 @@ public class SteamAPICalls {
 	
 	public static ObservableList<GameData> getOwnedGames() {
 		return playerGames;
+	}
+	
+	public static ObservableList<Friend> getFriendList() {
+		return friendList;
 	}
 	
 	public void init() {
@@ -60,8 +69,72 @@ public class SteamAPICalls {
 			}
 
 		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+			//e1.printStackTrace();
 		}
+	}
+	public void loadSteamFriends() {
+		URL url;
+		HttpURLConnection con;
+		try {
+			url = new URL("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=38FB06680EA5CA6B526B31CBD4E43593&steamid=76561197960435530&relationship=friend");
+			con = (HttpURLConnection)url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/json");
+			int responsecode = con.getResponseCode(); 
+
+			if(responsecode == 200)
+			{
+				String str="";
+				Scanner sc = new Scanner(url.openStream());
+				while(sc.hasNext())
+				{
+					str+=sc.nextLine();
+				}
+				sc.close();
+				System.out.println(str);
+				
+				//JSON string to Java Object			
+				FriendList response = mapper.readValue(str, FriendList.class);
+				friendList = FXCollections.observableArrayList(response.getFriends());
+				System.out.println("Friends: " + friendList.size());
+			}
+		} catch (IOException e1) {
+			//e1.printStackTrace();
+		}
+		for(Friend f:friendList) {
+			f.setPlayerProfile(loadSteamPlayerProfile(f.getSteamid()));
+		}
+	}
+	private PlayerProfile loadSteamPlayerProfile(String steamID) {
+		URL url;
+		HttpURLConnection con;
+		PlayerProfile profile = new PlayerProfile();
+		try {
+			String key = "38FB06680EA5CA6B526B31CBD4E43593";
+			String parameters = String.format("key=%s&steamid=%s", key,steamID);
+			url = new URL("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?"+parameters);
+			con = (HttpURLConnection)url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("Content-Type", "application/json");
+			int responsecode = con.getResponseCode(); 
+
+			if(responsecode == 200)
+			{
+				String str="";
+				Scanner sc = new Scanner(url.openStream());
+				while(sc.hasNext())
+				{
+					str+=sc.nextLine();
+				}
+				sc.close();
+				System.out.println(str);
+				
+				//JSON string to Java Object			
+				profile = mapper.readValue(str, PlayerProfile.class);
+			}
+		} catch (IOException e1) {
+			//e1.printStackTrace();
+		}
+		return profile;
 	}
 }
---
+++ src/main/java/com/ryhma6/maven/steambeater/model/steamAPI/Friend.java
@@ -0,0 +1,33 @@
+package com.ryhma6.maven.steambeater.model.steamAPI;
+
+public class Friend {
+	private String steamid;
+	private String relationship;
+	private String friend_since;
+	private PlayerProfile playerProfile;
+	
+	public String getSteamid() {
+		return steamid;
+	}
+	public void setSteamid(String steamid) {
+		this.steamid = steamid;
+	}
+	public String getRelationship() {
+		return relationship;
+	}
+	public void setRelationship(String relationship) {
+		this.relationship = relationship;
+	}
+	public String getFriend_since() {
+		return friend_since;
+	}
+	public void setFriend_since(String friend_since) {
+		this.friend_since = friend_since;
+	}
+	public PlayerProfile getPlayerProfile() {
+		return playerProfile;
+	}
+	public void setPlayerProfile(PlayerProfile playerProfile) {
+		this.playerProfile = playerProfile;
+	}
+}
