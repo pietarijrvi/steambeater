05aaa450bf42b11952f77613dec889c502eac98e
Jetro Saarti <jetro.saarti@metropolia.fi>
2020-03-07 13:58:41 +0200

    javaFX tests: prism.verbose (extra messages) off

--- src/test/java/com/ryhma6/maven/steambeater/view/JavaFXGameListTest.java
+++ src/test/java/com/ryhma6/maven/steambeater/view/JavaFXGameListTest.java
@@ -46,14 +46,12 @@ class JavaFXGameListTest extends ApplicationTest {
 
 	@BeforeAll
 	public static void setupSpec() throws Exception {
-		
 			System.setProperty("java.awt.headless", "true");
 			System.setProperty("testfx.robot", "glass");
 			System.setProperty("testfx.headless", "true");
 			System.setProperty("prism.order", "sw");
 			System.setProperty("prism.text", "t2k");
-			System.setProperty("prism.verbose", "true");
-			
+			//System.setProperty("prism.verbose", "true");	
 	}
 	
 	/**
