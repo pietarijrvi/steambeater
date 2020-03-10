f27a05cfce8fefa23659a87c2cf7365da2d04f67
Jetro Saarti <jetro.saarti@metropolia.fi>
2020-03-10 23:55:10 +0200

    Static preferences (temporary version, storing SteamID should be implemented securely/at least hashed).

--- src/main/java/com/ryhma6/maven/steambeater/model/UserPreferences.java
+++ src/main/java/com/ryhma6/maven/steambeater/model/UserPreferences.java
@@ -5,15 +5,15 @@ import java.util.prefs.Preferences;
 public class UserPreferences {
 	
 	// Defines the node where the preferences are stored
-	private Preferences prefs = Preferences.userRoot().node(this.getClass().getName());
-	String steamIDKey = "SteamID";
+	private static Preferences prefs = Preferences.userRoot();
+	private static final String steamIDKey = "SteamID";
 	
-	  public void setSteamID(String steamID) {    
+	  public static void setSteamID(String steamID) {    
 		prefs.put(steamIDKey, steamID);
 		System.out.println("Saved ID: " + prefs.get(steamIDKey, "null"));
 	  }
 	  
-	  public String getSteamID() {
+	  public static String getSteamID() {
 		  return prefs.get(steamIDKey, "null");
 	  }
 }
