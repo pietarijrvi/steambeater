ce57ce4317a37196a1feb7bffa8e4993c51cb81c
Jetro Saarti <jetro.saarti@metropolia.fi>
2020-03-10 23:51:08 +0200

    Added method for loading all game entries belonging to single user/SteamID.

--- src/main/java/com/ryhma6/maven/steambeater/model/DatabaseController.java
+++ src/main/java/com/ryhma6/maven/steambeater/model/DatabaseController.java
@@ -1,10 +1,21 @@
 package com.ryhma6.maven.steambeater.model;
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.criterion.Restrictions;
+
+import com.ryhma6.maven.steambeater.model.steamAPI.GameData;
 
 public class DatabaseController {
 	private SessionFactory sf;
@@ -14,10 +25,16 @@ public class DatabaseController {
 		sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
 	}
 	
-	public Boolean addGame(String gameID, String userID, Boolean beaten, Boolean unbeatable, Boolean ignored) {
+	public Boolean addGame(GameData game) {
 		try {
 			Session session = sf.openSession();
 			session.beginTransaction();
+			String gameID = String.valueOf(game.getAppid());
+			String userID = UserPreferences.getSteamID();
+			Boolean beaten = game.isBeaten();
+			Boolean unbeatable = game.isUnbeatable();
+			Boolean ignored = game.isIgnored();
+			//String gameID, String userID, Boolean beaten, Boolean unbeatable, Boolean ignored
 	
 			GameListEntry gle = new GameListEntry(gameID, userID, beaten, unbeatable, ignored);
 	
@@ -35,10 +52,32 @@ public class DatabaseController {
 			Session session = sf.openSession();
 			session.beginTransaction();
 			
-			GameListEntry gle = (GameListEntry) session.load(GameListEntry.class, gameID + userID);
+			GameListEntry gle = (GameListEntry) session.get(GameListEntry.class, gameID + userID);
 			session.getTransaction().commit();
 			
 			return gle;
+		} catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+		
+	}
+	
+	public List<GameListEntry> getAllUserGames(String userID) {
+		try {
+			Session session = sf.openSession();
+			session.beginTransaction();
+			
+			CriteriaBuilder builder = session.getCriteriaBuilder();
+			CriteriaQuery<GameListEntry> criteria = builder.createQuery(GameListEntry.class);		
+			Root<GameListEntry> gameList = criteria.from(GameListEntry.class);
+			Predicate predicate = builder.equal(gameList.get("userID"), userID);
+			criteria.where(predicate);	
+			List<GameListEntry> entries = session.createQuery(criteria).getResultList();
+			
+			session.getTransaction().commit();
+			
+			return entries;
 		} catch(Exception e) {
 			System.out.println(e);
 			return null;
