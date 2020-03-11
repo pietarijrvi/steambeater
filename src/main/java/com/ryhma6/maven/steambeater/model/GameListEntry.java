e3695c0854d46acdf7b9c0d471b98da9e41b6d99
kimmet <kimwi@metropolia.fi>
2020-03-11 21:17:24 +0200

    Comments filled in for classes I mainly worked on and removed pointless imports/attributes from them

--- src/main/java/com/ryhma6/maven/steambeater/model/GameListEntry.java
+++ src/main/java/com/ryhma6/maven/steambeater/model/GameListEntry.java
@@ -2,33 +2,67 @@ package com.ryhma6.maven.steambeater.model;
 
 import javax.persistence.*;
 
+/**
+ * Table where all the user set game data is saved in
+ * @author KimW
+ *
+ */
 @Entity
 @Table(name="GameList")
 public class GameListEntry {
 	
+	/**
+	 * Acts as the ID for the database table, formed from gameID and userID
+	 */
 	@Id
 	@Column(name="entryID")
 	private String entryID;
 	
+	/**
+	 * ID of the game
+	 */
 	@Column(name="gameID")
 	private String gameID;
 	
+	/**
+	 * Game owners ID
+	 */
 	@Column(name="userID")
 	private String userID;
 	
+	/**
+	 * Boolean indicating if the game is beaten
+	 */
 	@Column(name="beaten")
 	private Boolean beaten;
 	
+	/**
+	 * Boolean indicating if the game is unbeatable
+	 */
 	@Column(name="unbeatable")
 	private Boolean unbeatable;
 	
+	/**
+	 * Boolean indicating if the game is ignored
+	 */
 	@Column(name="ignored")
 	private Boolean ignored;
 	
+	/**
+	 * Creates an empty GameListEntry object
+	 */
 	public GameListEntry() {
 		super();
 	}
 	
+	/**
+	 * Creates a complete GameListEntry object
+	 * @param gameID
+	 * @param userID
+	 * @param beaten
+	 * @param unbeatable
+	 * @param ignored
+	 */
 	public GameListEntry(String gameID, String userID, Boolean beaten, Boolean unbeatable, Boolean ignored) {
 		super();
 		this.userID = userID;
@@ -39,46 +73,84 @@ public class GameListEntry {
 		this.entryID = gameID + userID;
 	}
 	
+	/**
+	 * sets the entryID using the object's gameID and userID, remember to set them before setting this
+	 */
 	public void setEntryID() {
 		this.entryID = gameID + userID;
 	}
 	
+	/**
+	 * @return Beaten status of the game
+	 */
 	public Boolean getBeaten() {
 		return beaten;
 	}
 
+	/**
+	 * Sets the beaten status of the game
+	 * @param beaten
+	 */
 	public void setBeaten(Boolean beaten) {
 		this.beaten = beaten;
 	}
 
+	/**
+	 * @return Unbeatable status of the game
+	 */
 	public Boolean getUnbeatable() {
 		return unbeatable;
 	}
 
+	/**
+	 * Sets the unbeatable status of the game
+	 * @param unbeatable
+	 */
 	public void setUnbeatable(Boolean unbeatable) {
 		this.unbeatable = unbeatable;
 	}
 
+	/**
+	 * @return Ignored status of the game
+	 */
 	public Boolean getIgnored() {
 		return ignored;
 	}
 
+	/**
+	 * Sets the ignored status of the game
+	 * @param ignored
+	 */
 	public void setIgnored(Boolean ignored) {
 		this.ignored = ignored;
 	}
 	
+	/**
+	 * @return game's ID 
+	 */
 	public String getGameID() {
 		return gameID;
 	}
 
+	/**
+	 *  Sets the game's ID
+	 * @param gameID
+	 */
 	public void setGameID(String gameID) {
 		this.gameID = gameID;
 	}
 
+	/**
+	 * @return user's ID
+	 */
 	public String getUserID() {
 		return userID;
 	}
 
+	/**
+	 * Sets the user's ID
+	 * @param userID
+	 */
 	public void setUserID(String userID) {
 		this.userID = userID;
 	}
--- src/main/java/com/ryhma6/maven/steambeater/view/StatComparisonController.java
+++ src/main/java/com/ryhma6/maven/steambeater/view/StatComparisonController.java
@@ -3,12 +3,9 @@ package com.ryhma6.maven.steambeater.view;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.ryhma6.maven.steambeater.MainApp;
 import com.ryhma6.maven.steambeater.model.SteamAPICalls;
-import com.ryhma6.maven.steambeater.model.steamAPI.GameData;
 import com.ryhma6.maven.steambeater.model.steamAPI.OwnedGames;
 
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -18,28 +15,58 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
+/**
+ * Controls the stat comparison panel, used by the FriendsListController
+ * @author KimW
+ *
+ */
 public class StatComparisonController implements Initializable {
-	private MainApp mainApp;
 	private SteamAPICalls api;
 	
+	/**
+	 * Deepest pane of the panel
+	 */
 	@FXML
 	private AnchorPane statAnchor; 
 	
+	/**
+	 * Button that closes the panel
+	 */
 	@FXML
 	private Button closeBtn;
 	
+	/**
+	 * Bar used to store the close button and used to lower the panel
+	 */
 	@FXML
 	private BorderPane topBar;
 
+	/**
+	 * Pane into which the friend's stats are put into
+	 */
 	@FXML
 	private AnchorPane friendStats;
 	
+	/**
+	 * Pane where the user's stats are put into
+	 */
 	@FXML
 	private AnchorPane userStats;
 	
-	int count;
-	Label friendLabel;
-	Label userLabel;
+	/**
+	 * Used to count the users games
+	 */
+	private int count;
+	
+	/**
+	 * Used to show the friend's stats
+	 */
+	private Label friendLabel;
+	
+	/**
+	 * Used to show the user's stats
+	 */
+	private Label userLabel;
 	
 	/***
 	 * Closes comparison stat window
@@ -57,6 +84,11 @@ public class StatComparisonController implements Initializable {
 		statAnchor.setVisible(true);
 	}
 	
+	/**
+	 * Loads the user's stats and the stats of the friend of which ID and name was given
+	 * @param friendsID
+	 * @param name
+	 */
 	public void loadStats(String friendsID, String name) {
 		OwnedGames fGames = api.loadFriendsGames(friendsID);
 		System.out.println("friend's games count: " + fGames.getGame_count());
@@ -66,6 +98,9 @@ public class StatComparisonController implements Initializable {
 		userLabel.setText("You own " + count + " games");
 	}
 
+	/**
+	 * Initializes the panel by loading in elements and makes sure the panel is closed by default
+	 */
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		
@@ -84,9 +119,5 @@ public class StatComparisonController implements Initializable {
         });
 		
 		closeComparison();
-	}
-	
-	public void setMainApp(MainApp mainApp) {
-		this.mainApp = mainApp;
 	}
 }
