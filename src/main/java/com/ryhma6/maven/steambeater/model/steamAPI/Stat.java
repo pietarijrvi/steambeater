43f182ed5f610d8087e89e278c5775257578ee20
Jetro Saarti <jetro.saarti@metropolia.fi>
2020-03-12 14:47:39 +0200

    added comments

--- src/main/java/com/ryhma6/maven/steambeater/model/steamAPI/Stat.java
+++ src/main/java/com/ryhma6/maven/steambeater/model/steamAPI/Stat.java
@@ -3,8 +3,37 @@ package com.ryhma6.maven.steambeater.model.steamAPI;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Object model of a single stat details from SteamAPI JSON response. SteamAPI
+ * documentation: https://developer.valvesoftware.com/wiki/Steam_Web_API
+ *
+ */
 public class Stat {
-	@Getter @Setter private String name;
-	@Getter @Setter private int defaultvalue;
-	@Getter @Setter private String displayName;
+	/**
+	 * Name of the stat (api name)
+	 * 
+	 * @param name Stat name in api format from SteamAPI
+	 * @return name Api name of the stat
+	 */
+	@Getter
+	@Setter
+	private String name;
+	/**
+	 * Default value of the stat (usually 0).
+	 * 
+	 * @param defaultValue Default value of the stat from SteamAPI
+	 * @return defaultValue Default value of the stat
+	 */
+	@Getter
+	@Setter
+	private int defaultvalue;
+	/**
+	 * Display name can be used in UI
+	 * 
+	 * @param displayName Display name of the stat from SteamAPI
+	 * @return displayName Display name of the stat
+	 */
+	@Getter
+	@Setter
+	private String displayName;
 }
