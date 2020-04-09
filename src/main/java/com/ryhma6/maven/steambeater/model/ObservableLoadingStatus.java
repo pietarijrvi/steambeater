8a5f4fe3f0066c7eb1538734dcc7f44d01ab874a
jsrti <56799866+jsrti@users.noreply.github.com>
2020-04-10 02:27:30 +0300

    Added comments

--- src/main/java/com/ryhma6/maven/steambeater/model/ObservableLoadingStatus.java
+++ src/main/java/com/ryhma6/maven/steambeater/model/ObservableLoadingStatus.java
@@ -5,20 +5,28 @@ import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
 /**
- * Contains the current loading status that can be observed in UI controls. Singleton.
+ * Contains the current loading status that can be observed in UI controls.
+ * Singleton.
  *
  */
 public class ObservableLoadingStatus {
 	private static final ObservableLoadingStatus INSTANCE = new ObservableLoadingStatus();
-	
+
 	/**
 	 * Contains the current loading status
 	 */
 	private ObjectProperty<LoadingStatus> loadingStatus = new SimpleObjectProperty<>();
-	
-	private long lastCompletionMillis; 
+
+	/**
+	 * Last successful api data load completion time
+	 */
+	private long lastCompletionMillis;
 	private boolean apiLoadFailure = false;
-	
+
+	/**
+	 * Sets a flag that shows that the api data was not loaded successfully so final
+	 * status will be failure.
+	 */
 	public void setApiLoadFailure() {
 		apiLoadFailure = true;
 	}
@@ -30,9 +38,11 @@ public class ObservableLoadingStatus {
 	public static ObservableLoadingStatus getInstance() {
 		return INSTANCE;
 	}
-	
+
 	/**
-	 * Returns the time stamp of the last loading completion (system time in milliseconds).
+	 * Returns the time stamp of the last loading completion (system time in
+	 * milliseconds).
+	 * 
 	 * @return time stamp
 	 */
 	public long getLastCompletionMillis() {
@@ -40,29 +50,31 @@ public class ObservableLoadingStatus {
 	}
 
 	/**
-	 * Set the current loading state. Can be set from any thread, events (notifying observers) are posted to event queue.
+	 * Set the current loading state. Can be set from any thread, events (notifying
+	 * observers) are posted to event queue.
+	 * 
 	 * @param currentState
 	 */
 	public void setLoadingStatus(LoadingStatus newStatus) {
-		if(newStatus==LoadingStatus.COMPLETED) {
+		if (newStatus == LoadingStatus.COMPLETED) {
 			lastCompletionMillis = System.currentTimeMillis();
-		}else if(newStatus==LoadingStatus.PRELOAD) {
-			//reset failure flag
+		} else if (newStatus == LoadingStatus.PRELOAD) {
+			// reset failure flag
 			apiLoadFailure = false;
 		}
-		
+
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
 				Platform.runLater(new Runnable() {
 					@Override
 					public void run() {
-						//if loading data has failed, set failure status instead of completion
-						if(apiLoadFailure && newStatus==LoadingStatus.COMPLETED) {
+						// if loading data has failed, set failure status instead of completion
+						if (apiLoadFailure && newStatus == LoadingStatus.COMPLETED) {
 							loadingStatus.setValue(LoadingStatus.FAILURE);
-						}else {
+						} else {
 							loadingStatus.setValue(newStatus);
-						}	
+						}
 					}
 				});
 			}
@@ -71,6 +83,7 @@ public class ObservableLoadingStatus {
 
 	/**
 	 * Get load state property that can be bound to observers
+	 * 
 	 * @return
 	 */
 	public ObjectProperty<LoadingStatus> getLoadingStateProperty() {
